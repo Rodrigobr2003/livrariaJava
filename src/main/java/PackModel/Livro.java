7ead49fb3dd07c1f59e7545651cbfb375926f798
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-08-23 12:09:12 -0300

    :sparkles: feat: Adiciona conteudo class Livro

--- src/main/java/PackModel/Livro.java
+++ src/main/java/PackModel/Livro.java
@@ -4,10 +4,64 @@
  */
 package PackModel;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Rodrigo
  */
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private LocalDate dataPublicacao;
+    private boolean emprestado = false;
     
+    public Livro(String titulo, String autor, String isbn, LocalDate dataPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.dataPublicacao = dataPublicacao;
+        this.emprestado = false;
+    }
+    
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    
+    public String getTitulo(){
+        return this.titulo;
+    }
+    
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    
+    public String getAutor(){
+        return this.autor;
+    }
+    
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+    
+    public String getIsbn(){
+        return this.isbn;
+    }
+    
+    public void setDataPublicacao(LocalDate dataPublicacao){
+        this.dataPublicacao = dataPublicacao;
+    }
+    
+    public LocalDate getDataPublicacao(){
+        return this.dataPublicacao;
+    }
+    
+    public void setEmprestado(boolean emprestado){
+        this.emprestado = emprestado;
+    }
+    
+    public boolean getEmprestado(){
+        return this.emprestado;
+    }
 }
