8b81f29726ea67dbc54ca6944b36024cd0d690cf
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-08-27 08:10:06 -0300

    :sparkles: feat: Adiciona conteudo classe User

--- src/main/java/PackModel/User.java
+++ src/main/java/PackModel/User.java
@@ -9,5 +9,42 @@ package PackModel;
  * @author Rodrigo
  */
 public class User {
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    public User(String nome, String email, String telefone){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setTetlefone(String telefone){
+        this.telefone = telefone;
+    }
+    
+    public String getTelefone(){
+        return this.telefone;
+    }
+    
+    public String getUser(){
+        return "Nome: " + this.nome + ", Email: " + this.email + " e Telefone: " + this.telefone;
+    }
     
 }
