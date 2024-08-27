a8b07cf7b46fd11b8c67368456719633e4571865
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-08-27 08:20:38 -0300

    :sparkles: feat: Adiciona conteudo classe Emprestimo

--- src/main/java/PackModel/Emprestimo.java
+++ src/main/java/PackModel/Emprestimo.java
@@ -4,10 +4,58 @@
  */
 package PackModel;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Rodrigo
  */
 public class Emprestimo {
+    private Livro livro;
+    private User usuario;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
     
+    public Emprestimo(Livro livro, User usuario, LocalDate dataEmprestimo, LocalDate dataDevolucao){
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+    
+    public void setLivro(Livro livro){
+        this.livro = livro;
+    }
+    
+    public Livro getLivro(){
+        return this.livro;
+    }
+    
+    public void setUsario(User usuario){
+        this.usuario = usuario;
+    }
+    
+    public User getUsuario(){
+        return this.usuario;
+    }
+    
+    public void setDataEmprestimo(LocalDate dataEmprestimo){
+        this.dataEmprestimo = dataEmprestimo;
+    }
+    
+    public LocalDate getDataEmprestimo(){
+        return this.dataEmprestimo;
+    }
+    
+    public void setDataDevolucao(LocalDate dataDevolucao){
+        this.dataDevolucao = dataDevolucao;
+    }
+    
+    public LocalDate getDataDevolucao(){
+        return this.dataDevolucao;
+    }
+    
+    public String getEmprestimo(){
+        return "Livro: " + this.livro + ", Usuario: " + this.usuario + ", Data emprestimo: " + this.dataEmprestimo + " e Data devolucao: " + this.dataDevolucao;
+    }
 }
